7cd7f58ea32aec8ce0f0e3a83fc8671e117dd8a7
John Greibe Larsen <johnglarsen@hotmail.co.uk>
2021-04-24 10:18:15 +0100

    Added tracking of nodes expanded and new evaluation functions

--- my_custom_player.py
+++ my_custom_player.py
@@ -1,6 +1,9 @@
-
+import random
 from sample_players import DataPlayer
 
+# TODO import from issolation
+_WIDTH = 11
+_HEIGHT = 9
 
 class CustomPlayer(DataPlayer):
     """ Implement your own agent to play knight's Isolation
@@ -19,6 +22,9 @@ class CustomPlayer(DataPlayer):
       any pickleable object to the self.context attribute.
     **********************************************************************
     """
+
+    nodes = 0
+
     def get_action(self, state):
         """ Employ an adversarial search technique to choose an action
         available in the current state calls self.queue.put(ACTION) at least
@@ -36,11 +42,120 @@ class CustomPlayer(DataPlayer):
           Refer to (and use!) the Isolation.play() function to run games.
         **********************************************************************
         """
-        # TODO: Replace the example implementation below with your own search
-        #       method by combining techniques from lecture
-        #
-        # EXAMPLE: choose a random move without any search--this function MUST
-        #          call self.queue.put(ACTION) at least once before time expires
-        #          (the timer is automatically managed for you)
-        import random
-        self.queue.put(random.choice(state.actions()))
+        #print(f"Start of meethoodd: Nodes: {CustomPlayer.nodes}, self: {self}")
+        if state.ply_count < 2:
+            self.queue.put(random.choice(state.actions()))
+        else:
+            depth_limit = 5
+            try:
+                for depth in range(depth_limit, depth_limit + 1):
+                    self.queue.put((self.alpha_beta_search(state, depth), CustomPlayer.nodes))
+            except Exception:  # At deeper levels (depths) we will experience time out exception - ignore
+                pass
+        #print(f"End of method: Nodes: {CustomPlayer.nodes}, self: {self}")
+
+    def alpha_beta_search(self, gameState, depth_limit):
+        """ Return the move along a branch of the game tree that
+        has the best possible value. A move is a pair of coordinates
+
+        in (column, row) order corresponding to a legal move for
+        the searching player.
+        """
+
+        def min_value(gameState, alpha, beta, depth_limit):
+            CustomPlayer.nodes += 1
+
+            """ Return he utility value of the current game state for the specified
+            player. The game has a utility of +inf if the player has won, a value of
+            -inf if the player has lost, and a value of 0 otherwise.
+            """
+
+            if gameState.terminal_test():
+                return gameState.utility(self.player_id)
+
+            if depth_limit <= 0:
+                #return player_vs_opp_moves(gameState)
+                #return offensive_to_defensive(gameState, 3)
+                #return offensive(gameState, 3)
+                #return defensive(gameState, 3)
+                return defensive_to_offensive(gameState, 3)
+
+            v = float("inf")
+            for a in gameState.actions():
+                v = min(v, max_value(gameState.result(a), alpha, beta, depth_limit - 1))
+                if v <= alpha:
+                    return v
+                beta = min(beta, v)
+            return v
+
+        def max_value(gameState, alpha, beta, depth_limit):
+            CustomPlayer.nodes += 1
+
+            """ Return he utility value of the current game state for the specified
+            player. The game has a utility of +inf if the player has won, a value of
+            -inf if the player has lost, and a value of 0 otherwise.
+            """
+            if gameState.terminal_test():
+                return gameState.utility(self.player_id)
+
+            if depth_limit <= 0:
+                #return player_vs_opp_moves(gameState)
+                #return offensive_to_defensive(gameState, 3)
+                #return offensive(gameState, 3)
+                #return defensive(gameState, 3)
+                return defensive_to_offensive(gameState, 3)
+
+
+            v = float("-inf")
+            for a in gameState.actions():
+                v = max(v, min_value(gameState.result(a), alpha, beta, depth_limit - 1))
+                if v >= beta:
+                    return v
+                alpha = max(alpha, v)
+            return v
+
+        def player_liberties(gameState):
+            loc_player = gameState.locs[self.player_id]
+            loc_opp = gameState.locs[1 - self.player_id]
+            return (gameState.liberties(loc_player), gameState.liberties(loc_opp))
+
+        def baseline(gameState):
+            lib_player, lib_opp = player_liberties(gameState)
+            return len(lib_player) - len(lib_opp)
+
+        def offensive(gameState, weight):
+            lib_player, lib_opp = player_liberties(gameState)
+            return len(lib_player) - weight*len(lib_opp)
+
+        def defensive(gameState, weight):
+            lib_player, lib_opp = player_liberties(gameState)
+            return weight*len(lib_player) - len(lib_opp)
+
+        def offensive_to_defensive(gameState, weight):
+            board_fields_occcupied = gameState.ply_count / (_WIDTH * _HEIGHT)
+            if board_fields_occcupied <= 0.50:
+                return offensive(gameState, weight)
+            else:
+                return defensive(gameState, weight)
+
+        def defensive_to_offensive(gameState, weight):
+            board_fields_occcupied = gameState.ply_count / (_WIDTH * _HEIGHT)
+            if board_fields_occcupied <= 0.50:
+                return defensive(gameState, weight)
+            else:
+                return offensive(gameState, weight)
+
+
+        CustomPlayer.nodes += 1
+        alpha = float("-inf")
+        beta = float("inf")
+        best_score = float("-inf")
+        # Important initialise best_move: makes testing the solution stable by avoiding returning None in a losing game
+        best_move = gameState.actions()[0]
+        for a in gameState.actions():
+            v = min_value(gameState.result(a), alpha, beta, depth_limit - 1)
+            alpha = max(alpha, v)
+            if v > best_score:
+                best_score = v
+                best_move = a
+        return best_move
